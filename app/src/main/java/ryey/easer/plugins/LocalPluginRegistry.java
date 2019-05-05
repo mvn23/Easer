554ab0821466dc926b2d6039a2fea3029a3f3f84
mvn23 <schopdiedwaas@gmail.com>
2019-05-06 01:18:29 +0200

    Implement calendar condition plugin with support for Event Title pattern matching. All-day events not yet implemented.

--- app/src/main/java/ryey/easer/plugins/LocalPluginRegistry.java
+++ app/src/main/java/ryey/easer/plugins/LocalPluginRegistry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 - 2018 Rui Zhao <renyuneyun@gmail.com>
+ * Copyright (c) 2016 - 2019 Rui Zhao <renyuneyun@gmail.com>
  *
  * This file is part of Easer.
  *
@@ -46,6 +46,7 @@ import ryey.easer.commons.local_plugin.operationplugin.OperationPlugin;
 import ryey.easer.plugins.condition.battery.BatteryConditionPlugin;
 import ryey.easer.plugins.condition.bluetooth_device.BTDeviceConditionPlugin;
 import ryey.easer.plugins.condition.bluetooth_enabled.BluetoothEnabledConditionPlugin;
+import ryey.easer.plugins.condition.calendar.CalendarConditionPlugin;
 import ryey.easer.plugins.condition.cell_location.CellLocationConditionPlugin;
 import ryey.easer.plugins.condition.connectivity.ConnectivityConditionPlugin;
 import ryey.easer.plugins.condition.date.DateConditionPlugin;
@@ -133,6 +134,7 @@ final public class LocalPluginRegistry {
 
         condition().registerPlugin(BatteryConditionPlugin.class);
         condition().registerPlugin(BTDeviceConditionPlugin.class);
+        condition().registerPlugin(CalendarConditionPlugin.class);
         condition().registerPlugin(CellLocationConditionPlugin.class);
         condition().registerPlugin(ConnectivityConditionPlugin.class);
         condition().registerPlugin(DateConditionPlugin.class);
